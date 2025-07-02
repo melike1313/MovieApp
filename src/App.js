0238492c750c2b94a4c12a46cfd6001def8e05b6
melike <melikenogay13@gmail.com>
2025-07-01 22:51:55 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./MovieCard";
-//d482c5e
+
 const API_URL = "https://www.omdbapi.com?apikey=d482c5e";
 
-const movie1 = {
-  Title: "The Avengers",
-  Year: "2012",
-  imdbID: "tt0848228",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg",
-};
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -19,15 +11,22 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    setMovies(data.Search);
+
+    if (data.Response === "True") {
+      setMovies(data.Search);
+    } else {
+      setMovies([]); // Film bulunamadıysa boş liste döndür
+    }
   };
 
   useEffect(() => {
     searchMovies("Avengers");
   }, []);
+
   return (
     <div className="app">
       <h1>MovieLand</h1>
+
       <div className="search">
         <input
           placeholder="Search for movies"
@@ -40,6 +39,7 @@ const App = () => {
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
+
       {movies.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
